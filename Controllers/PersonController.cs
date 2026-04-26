9287f1ea0e0f9675b2027e33a5963cae218406d2
Darun Karas Abir <darun15-14188@diu.edu.bd>
2026-04-26 12:11:26 +0600

    create all endpoint

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -6,7 +6,7 @@ namespace person_management_system.Controllers
     using person_management_system.Models;
     using person_management_system.Services;
 
-    [Route("api/[controller]")]
+    [Route("api/persons")]
     [ApiController]
     public class PersonController : ControllerBase
     {
@@ -33,6 +33,79 @@ namespace person_management_system.Controllers
         }
 
 
+        //get all persons
+        [HttpGet]
+        public async Task<IActionResult> GetAllPersons()
+        {
+            try
+            {
+                var result = await _personService.GetAllPersonsAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
 
+
+        }
+
+        // get person by id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPersonById(Guid id)
+        {
+            try
+            {
+                var result = await _personService.GetPersonByIdAsync(id);
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //delete person by id
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePersonById(Guid id)
+        {
+            try
+            {
+                var result = await _personService.DeletePersonAsync(id);
+
+                if (!result)
+                    return NotFound();
+
+                return NoContent(); // best practice for DELETE
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        //update person by id
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePersonById(Guid id, PersonCreateDto personData)
+        {
+            try
+            {
+                var result = await _personService.UpdatePersonAsync(id, personData);
+                if (result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
     }
 }
--- Services/PersonService.cs
+++ Services/PersonService.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using person_management_system.DTOs;
+using person_management_system.Models;
 
 namespace person_management_system.Services
 {
@@ -18,7 +21,8 @@ namespace person_management_system.Services
             _context = context;
         }
 
-        public async Task<Models.Person> AddPersonAsync(PersonCreateDto personData)
+        // create a new person
+        public async Task<Person> AddPersonAsync(PersonCreateDto personData)
         {
             var person = new Models.Person
             {
@@ -31,6 +35,75 @@ namespace person_management_system.Services
             await _context.SaveChangesAsync();
 
             return person;
+        }
+
+
+        // get all persons
+        public async Task<List<PersonGetDto>> GetAllPersonsAsync()
+        {
+            return await _context.Persons
+                .Select(p => new PersonGetDto
+                {
+                    Id = p.Id,
+                    FirstName = p.FirstName,
+                    LastName = p.LastName,
+                    BirthDate = p.BirthDate,
+                    CreatedAt = p.CreatedAt
+                })
+                .ToListAsync();
+        }
+
+
+        //Find by id
+        public async Task<PersonGetDto?> GetPersonByIdAsync(Guid id)
+        {
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null) return null;
+
+            return new PersonGetDto
+            {
+                Id = person.Id,
+                FirstName = person.FirstName,
+                LastName = person.LastName,
+                BirthDate = person.BirthDate,
+                CreatedAt = person.CreatedAt
+            };
+
+        }
+
+
+        //delete person
+        public async Task<bool> DeletePersonAsync(Guid id)
+        {
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null) return false;
+
+            _context.Persons.Remove(person);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+
+        //update person
+        public async Task<PersonGetDto?> UpdatePersonAsync(Guid id, PersonCreateDto updatedData)
+        {
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null) return null;
+
+            person.FirstName = updatedData.FirstName;
+            person.LastName = updatedData.LastName;
+            person.BirthDate = updatedData.BirthDate;
+
+            await _context.SaveChangesAsync();
+
+            return new PersonGetDto
+            {
+                Id = person.Id,
+                FirstName = person.FirstName,
+                LastName = person.LastName,
+                BirthDate = person.BirthDate,
+                CreatedAt = person.CreatedAt
+            };
         }
     }
 }
